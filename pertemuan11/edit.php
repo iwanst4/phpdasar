b8f258b3e947b4f2eddafe8f83c3465521970706
iwansetiawan <iwansetiawan@smabudiutomoperak.sch.id>
2023-10-12 22:23:35 +0700

    Pertemuan 11 - Update data

---
+++ pertemuan11/edit.php
@@ -0,0 +1,73 @@
+<?php 
+require 'functions.php';
+
+// ambil data pada url
+$id = $_GET["id"];
+
+// query data berdasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+// cek apakah tombol submit sudah ditekan
+if( isset($_POST["submit"]) ) {
+
+    // cek apakah data berhasil diubah atau tidak
+    if( edit($_POST) > 0 ) {
+        echo "
+        <script>
+            alert('Data berhasil diubah!');
+            document.location.href = 'index.php';
+        </script>
+        ";
+    } else {
+        echo "
+        <script>
+            alert('Data gagal diubah!');
+            document.location.href = 'index.php';
+        </script>
+        ";
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit data mahasiswa</title>
+</head>
+<body>
+    <h1>Edit data mahasiswa</h1>
+    <form action="" method="post">
+        <input type="hidden" name="id" value="<?= $mhs["id"] ?>">
+        <ul>
+            <li>
+                <label for="nrp">NRP :</label>
+                <input type="text" name="nrp" id="nrp" require value="<?= $mhs["nrp"] ?>">
+            </li>
+            <li>
+                <label for="nama">Nama :</label>
+                <input type="text" name="nama" id="nama" require value="<?= $mhs["nama"] ?>">
+            </li>
+            <li>
+                <label for="email">Email :</label>
+                <input type="text" name="email" id="email" require value="<?= $mhs["email"] ?>">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan :</label>
+                <input type="text" name="jurusan" id="jurusan" require value="<?= $mhs["jurusan"] ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar :</label>
+                <input type="text" name="gambar" id="gambar" value="<?= $mhs["gambar"] ?>">
+            </li>
+            <li>
+                <button type="submit" name="submit">Edit Data</button>
+            </li>
+        </ul>
+   
+    </form>
+</body>
+</html>
